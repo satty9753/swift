2c8b88734735a9be0f631b9fb500b7ec67015213
Michelle Chen <you@example.com>
2017-06-16 13:42:29 +0800

    Initial Commit

---
+++ GirlFrotierAsk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GirlFrotierAsk
+//
+//  Created by Michelle Chen on 2017/6/16.
+//  Copyright © 2017年 Michelle Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
